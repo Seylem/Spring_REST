93e5b88e19a3ead3cc9db90f2a0c16c9f6c478c0
DPolukhin <dpolukhin@luxoft.com>
2022-03-15 21:41:44 +0300

    add UserDto

---
+++ src/main/java/web/model/dto/UserDto.java
@@ -0,0 +1,30 @@
+package web.model.dto;
+
+import lombok.Data;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Data
+public class UserDto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "firstname")
+    private String firstname;
+
+    @Column(name = "lastname")
+    private String lastname;
+
+    @Column(name = "age")
+    private Integer age;
+
+    @Column(name = "mail")
+    private String mail;
+
+    @Column(name = "password")
+    private String password;
+}
