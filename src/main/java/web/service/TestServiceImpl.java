ca96498420a09b5e1fff1ed8bdd9b5d5f4b1b297
DPolukhin <dpolukhin@luxoft.com>
2022-03-21 18:54:34 +0300

    add TestService and TestServiceImpl

---
+++ src/main/java/web/service/TestServiceImpl.java
@@ -0,0 +1,21 @@
+package web.service;
+
+import java.util.List;
+
+public class TestServiceImpl {
+    private List<String> list;
+
+    public List<String> getList() {
+        return list;
+    }
+
+    public void initList() {
+        list.add("один");
+        list.add("два");
+        list.add("три");
+        list.add("четыре");
+        list.add("пять");
+        list.add("шесть");
+        list.add("семь");
+    }
+}
